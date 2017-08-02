7a0097c7eeb397c521e54a6b80c9c15432dd3de5
Madhan Raju J <Madhan.J@BRILLIO.COM>
2017-08-02 19:22:02 +0530

    updated

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var restify = require('restify');
+var YQL     = require('yql');
 var builder = require('botbuilder');
 
 // Setup Restify Server
@@ -188,6 +189,103 @@ function reviewAsAttachment(review) {
         .text(review.text)
         .images([new builder.CardImage().url(review.image)]);
 }
+
+/** Fetch the weather forecast for a city */
+bot.dialog('GetForecast', [
+  function(session, args, next) {
+    const location = builder.EntityRecognizer.findEntity(args.entities, 'builtin.geography.city');
+    const timeperiod = builder.EntityRecognizer.findEntity(args.entities, 'builtin.datetime.date');
+
+    if (!location) {
+      builder.Prompts.text(session, 'Where?');
+    } else {
+      next({
+        location: location.entity,
+        timeperiod: timeperiod
+      });
+    }
+  },
+  function(session, results) {
+    const loc = results.location;
+
+    weatherForecast(loc, (err, data) => {
+      const res = data.results.channel.item.forecast;
+
+      if (!results.timeperiod) {
+
+        res.forEach((item) => {
+          var message = item.day + ': ' + item.text + ' with a high of ' + item.high + ' and a low of ' + item.low;
+          session.send(message);
+        });
+
+      } else {
+
+        var forecastdate = moment(results.timeperiod.resolution.date, 'YYYY-MM-DD');
+        var forecast = forecastForADate(forecastdate, res);
+
+        if (forecast) {
+          var msg = forecast.day + ': ' + forecast.text + ' with a high of ' + forecast.high + ' and a low of ' + forecast.low;
+          session.send(msg);
+        } else {
+          var msg = "Whoops, forecast not available yet!";
+          session.send(msg);
+        }
+      }
+    });
+  }
+]);
+
+
+
+/*********************************************
+
+
+/**
+ * Fetch weather details from Yahoo Weather API
+ * @param loc Location to get the weather for
+ */
+function weatherForecast(loc, cb) {
+  const query = new YQL('select * from weather.forecast where woeid in (select woeid from geo.places(1) where text="' + loc + '")');
+
+  query.exec((err, data) => {
+    if (err) return cb(err);
+
+    return cb(null, data.query);
+  });
+}
+
+/**
+ * Find forecast for a specified date (if available)
+ */
+function forecastForADate(forecastDate, forecasts) {
+  return forecasts.find((item) => {
+    var date = moment(item.date, 'DD MMM YYYY');
+    return date.isSame(forecastDate);
+  });
+}
+
+/** Prompts to get the current weather conditions */
+bot.dialog('GetCurrentWeather', [
+  function(session, args, next) {
+    const location = builder.EntityRecognizer.findEntity(args.entities, 'builtin.geography.city');
+
+    if (!location) {
+      builder.Prompts.text(session, 'Where?');
+    } else {
+      next({
+        response: location.entity
+      });
+    }
+  },
+  function(session, results) {
+    const loc = results.response;
+
+    weatherForecast(loc, (err, data) => {
+      var res = data.results.channel.item.condition;
+      session.send(res.text + ' with a temperature of ' + res.temp + ' degress');
+    });
+  }
+]);
 
 /** Fetch the weather forecast for a city */
 bot.dialog('GetForecast', [
