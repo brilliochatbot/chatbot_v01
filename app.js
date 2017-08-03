3878b0c9d32176be6f161aecafd72302870c075a
Madhan Raju J <Madhan.J@BRILLIO.COM>
2017-08-03 17:40:53 +0530

    updated

--- app.js
+++ app.js
@@ -124,7 +124,7 @@ bot.dialog('carregistered', [
   
   //if (modelEntity === 'Lexus' ||  modelEntity === 'lexus')
 	{
-	//session.send('model %s', args[0])
+	session.send('model %d %s', carnumberEntity, carnumberEntity)
 	builder.Prompts.text(session, 'Fine. How can I help you today?');
 	}
 	else
