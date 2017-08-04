87bd79c75b680657dd10574432b6254538653aeb
Madhan Raju J <Madhan.J@BRILLIO.COM>
2017-08-04 15:43:17 +0530

    updated

--- app.js
+++ app.js
@@ -262,13 +262,13 @@ bot.dialog('carregistered', [
   
   var movearoundtypeEntity = builder.EntityRecognizer.findEntity(args.intent.entities, 'movearoundtype');
   
-  //if(movearoundtypeEntity)
+  if(movearoundtypeEntity)
   
   //if (modelEntity === 'Lexus' ||  modelEntity === 'lexus')
-	//{
-		session.send('model %s', results.response)
+	{
+		session.send('you selected %s', results.response)
 		//builder.Prompts.text(session, 'While we service your car, how do you prefer to move around?\n\nLoaner Car/ Shuttle service');
-		if(results.response == "loaner car")
+		/*if(results.response == "loaner car")
 		{
 		builder.Prompts.text(session,"Your loaner car would be available by 9:05 am and needs to be returned while taking your car back./ Driver named John will pick you by 9:15 am. You can reach him at (541) 754-3010)");
 		} else if(results.response == "shuttle service")
@@ -278,28 +278,30 @@ bot.dialog('carregistered', [
 		else 
 		{
 		builder.Prompts.text(session, 'please enter valid input');
-		}
-	//}
-	/*else
+		}*/
+	}
+	else
 	{
 	  //user
 	  //session.send('model %s', args[0])
 	  //session.send('model %s', args.intent.entities)
 	  //builder.Prompts.text(session,modelEntity);
 	builder.Prompts.text(session, 'please enter valid input');
-	}*/
+	}
   
    //session.send('Let me know your car number');
-  }/*,function(session, results) 
+  },function(session, results) 
 	{
-	if(results.response =="loaner car")
-	{
+		if(results.response =="loaner car")
+		{
 		builder.Prompts.text(session,"Your loaner car would be available by 9:05 am and needs to be returned while taking your car back./ Driver named John will pick you by 9:15 am. You can reach him at (541) 754-3010)");
-	} else if(results.response == "shuttle service")
+		} else if(results.response == "shuttle service")
 		{
 			builder.Prompts.text(session,"Driver named John will pick you by 9:15 am. You can reach him at (541) 754-3010)");
 		}
-	}*/
+		else
+		builder.Prompts.text(session, 'please enter valid input');
+	}
 		
     
   /*,  to get user ans
