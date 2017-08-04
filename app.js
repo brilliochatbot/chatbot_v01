51e95ded47f8b052a38459e83075f8f6bd01d362
Madhan Raju J <Madhan.J@BRILLIO.COM>
2017-08-04 15:48:24 +0530

    updated

--- app.js
+++ app.js
@@ -266,7 +266,8 @@ bot.dialog('carregistered', [
   
   //if (modelEntity === 'Lexus' ||  modelEntity === 'lexus')
 	{
-		session.send('you selected %s', results.response)
+		builder.Prompts.text(session, 'you are going with');
+		//session.send('you selected %s', results.response)
 		//builder.Prompts.text(session, 'While we service your car, how do you prefer to move around?\n\nLoaner Car/ Shuttle service');
 		/*if(results.response == "loaner car")
 		{
